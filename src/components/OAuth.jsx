63d4a04f7e2b4a3de88d88ec793bee8b7d97ef70
Dimasajalah <89116262+Dimasajalah@users.noreply.github.com>
2025-06-21 19:02:56 +0700

    Update OAuth.jsx

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -19,7 +19,7 @@ export default function OAuth() {
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
 
-      const res = await fetch('/api/auth/google', {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/google`, {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   credentials: 'include',
